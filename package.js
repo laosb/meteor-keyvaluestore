98cf70fe43e403df345bcc181ecabff07db61ebb
Peter Pal Koszta <peter.koszta@gfk.com>
2015-09-21 16:49:47 +0200

    Bump version to 0.1.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* Information about this package */
 Package.describe({
   summary: "Key-value store using a Meteor collection.",
-  version: "0.1.3",
+  version: "0.1.4",
   name: "gfk:keyvaluestore",
   git: "https://github.com/gfk-ba/meteor-keyvaluestore"
 });
